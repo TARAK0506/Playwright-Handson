5fa705c02bf669e2565f387472921027eb4e1314
TARAKA RAMARAO <tarakaramarao0506@gmail.com>
2025-07-15 15:06:12 +0530

    changes modified

--- utils/xlsReader.js
+++ utils/xlsReader.js
@@ -1,7 +1,20 @@
 // Importing the XLSX Library
 const xlsx = require('xlsx');
+const fs = require('fs');
+const path = require('path');
 
+// // const filePath = '../data/input-data.xlsx';
+const filePath = path.resolve(__dirname, '../data/input-data.xlsx');
+// // console.log('Reading Excel file from:', filePath);
+// const workbook = xlsx.readFile(filePath);
+// const sheetName = workbook.SheetNames[0]; 
+// const worksheet = workbook.Sheets[sheetName];
+// console.log(workbook);
+// console.log(sheetName);
+// console.log(worksheet);
+// console.log(xlsx.utils.sheet_to_json(worksheet));
 
+// console.log(filePath);
 // Read data from an Excel file
 function readExcelData(filePath) {
 
